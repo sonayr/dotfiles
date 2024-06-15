656502ddea6902eb8704c0f4cfa8f494e11f2b7c
Ryan <ryano@Ryans-MacBook-Pro.local>
2024-06-15 18:20:51 -0300

    Intitalize

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Install Home Brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install  brew apps
+xargs brew install < ./brew.txt

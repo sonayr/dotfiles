84ee59005616ad2aa5a45264f988a0686d65595a
Ryan O'Sullivan <nesto-ryan@Ryans-MacBook-Pro-2.local>
2024-10-28 16:29:57 -0300

    add a remove to get rid of the file after delete

--- scripts/.local/bin/deleteFlows.sh
+++ scripts/.local/bin/deleteFlows.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 apiName=$(basename $1 | grep -E "^[^.]+" -o)
-sf force data soql query --query "SELECT Id FROM Flow WHERE Definition.developername = '$apiName' AND Status != 'Active'" --use-tooling-api --result-format csv > flowTodelete.csv
+sf force data soql query \
+    --query "SELECT Id FROM Flow WHERE Definition.developername = '$apiName' AND Status != 'Active'"\
+    --use-tooling-api \
+    --result-format csv > delete_$apiName.csv
 while read c; do
     if [[ "$c" != "Id" && "$c" != "Your query returned no results." ]]
         then
             sf data record delete --sobject Flow --record-id $c --use-tooling-api
     fi
-done < flowTodelete.csv
-rm flowTodelete.csv
+done < delete_$apiName.csv
+rm delete_$apiName.csv 
+rm $1

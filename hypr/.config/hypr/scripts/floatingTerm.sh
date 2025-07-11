a777a69b6bfe27de87d05fd712b4c442fb4d19ea
Ryan O'Sullivan <ryanosullivan360@gmail.com>
2025-07-10 23:44:12 -0300

    Add scripts for ocarina button

---
+++ hypr/.config/hypr/scripts/floatingTerm.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+hyprctl dispatch exec '[float] ghostty --window-height=10 --window-width=80 --cursor-style-blink=false --cursor-style=block --cursor-opacity=0 -e .local/bin/ocarina.sh'
